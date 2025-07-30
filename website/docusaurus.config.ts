cb063b7de063481eaa4de8ec0717979a892cfa8e
hugocornellier <hugo.cornellier@gmail.com>
2025-07-30 13:58:10 -0300

    docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
       ],
     },
@@ -77,7 +77,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Documentation',
               to: 'docs/intro',
             },
           ],
