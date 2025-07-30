79f01ea5f12ef040a1b9554ed99aa6b9ba9ba40c
hugocornellier <hugo.cornellier@gmail.com>
2025-07-30 15:14:03 -0300

    docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -105,7 +105,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: ` `,
     },
     prism: {
       theme: prismThemes.github,
