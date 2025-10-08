ad9eba07f284f6d9f45b8dc722d4dca1fb2beb5c
hugocornellier <hugo.cornellier@gmail.com>
2025-10-08 10:22:05 -0300

    macos v2.1.2

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -370,7 +370,7 @@ export default function Home(): JSX.Element {
                                         >
                                             <div>MacOS</div>
                                             <Link
-                                                href="https://github.com/hugocornellier/agelapse/releases/download/agelapse-mac-2.1.1/AgeLapse-v2.1.1.zip"
+                                                href="https://github.com/hugocornellier/agelapse/releases/download/agelapse-mac-2.1.2/AgeLapse-v2.1.2.zip"
                                                 onMouseEnter={() => setDesktopBtnHover1(true)}
                                                 onMouseLeave={() => setDesktopBtnHover1(false)}
                                                 onFocus={() => setDesktopBtnHover1(true)}
